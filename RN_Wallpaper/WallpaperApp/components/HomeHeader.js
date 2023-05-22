3f5b553e6ef7bdd023833aee19e6193ac4d8edd6
joerylabie <joerylabie@hotmail.com>
2023-05-22 13:35:45 +0200

    Add small changes

--- RN_Wallpaper/WallpaperApp/components/HomeHeader.js
+++ RN_Wallpaper/WallpaperApp/components/HomeHeader.js
@@ -7,7 +7,7 @@ function HomeHeader(){
     <View style={styles.flexRow}>
         <View style={{marginLeft:13}}>
             <Text style={{fontSize:20}}>Hoi, Gebruiker!</Text>
-            <Text style={{fontSize:13}}>Tijd om notities te nemen?</Text>
+            <Text style={{fontSize:13}}>Bekijk de nieuwste wallpapers!</Text>
         </View>
         <View>
             <Entypo name="dot-single" size={70} color="black" style={{position:"absolute", left:45, top:0, zIndex:2}} />
